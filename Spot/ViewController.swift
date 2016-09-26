9f776d6fcbc696a9ad1d76b05310aa5dc1b5323e
umoruj <umoru80@gmail.com>
2016-09-26 13:23:05 +0200

    first initial commit

--- Spot/ViewController.swift
+++ Spot/ViewController.swift
@@ -7,12 +7,61 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
+    
+    var names = [String]()
+    
+    var searchUrl: String = "https://api.spotify.com/v1/search?q=Shawn+Mendes&type=track"
+    
+    typealias JSONStandard = [String: AnyObject]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        callAlamo(url: searchUrl)
+    }
+    
+    func callAlamo(url: String){
+        Alamofire.request(url).responseJSON(completionHandler: {
+            response in
+            self.parseData(JSONData: response.data!)
+        })
+    }
+    
+    func parseData(JSONData: Data){
+        do {
+            var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandard
+            if let tracks = readableJSON["tracks"] as? JSONStandard {
+                if let items = tracks["items"]{
+                    for i in 0..<items.count {
+                        let item = items[i] as! JSONStandard
+                        
+                        let name = item["name"] as! String
+                        
+                        names.append(name)
+                        
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = names[indexPath.row]
+        
+        return cell!
     }
 
     override func didReceiveMemoryWarning() {
